a1b98c44c8b1b112956c4ed114fc85c7acd8385d
Jaesung <bbbjason@naver.com>
2022-11-09 13:41:07 +0700

    bg Img setting on RequestAnimationFrame

--- main.js
+++ main.js
@@ -8,3 +8,31 @@ canvas.width=400;
 canvas.height=700;
 
 document.body.appendChild(canvas);
+
+let bgImg,spaceshipImg,bulletImg,enemyImg,gameoverImg;
+
+function imgLoad(){
+    bgImg = new Image();
+    spaceshipImg = new Image();
+    bulletImg = new Image();
+    enemyImg = new Image();
+    gameoverImg = new Image();
+
+    bgImg.src ='/imgs/background.jfif';
+    spaceshipImg.src='/imgs/spaceship.png';
+    bulletImg.src ='/imgs/bullet.png';
+    enemyImg.src = '/imgs/enemy.png';
+    gameoverImg.src ='/imgs/gameover.png';
+}
+
+function render(){
+    ctx.drawImage(bgImg,0,0, canvas.width, canvas.height); 
+}
+function main(){
+    render()
+    requestAnimationFrame(main)
+
+}
+
+imgLoad();
+main();
